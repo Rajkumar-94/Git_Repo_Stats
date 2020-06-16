514b517b152c1c4403188bd27af9de4f2d9c65f1
RajkumarQxf2 <58762673+RajkumarQxf2@users.noreply.github.com>
2020-06-16 20:00:56 +0530

    Splitted the method into smaller methods (#11)
    Splitted the method into smaller methods

--- Collect_Git_stat/test_to_run_on_lambda_function.py
+++ Collect_Git_stat/test_to_run_on_lambda_function.py
@@ -19,27 +19,15 @@ def lambda_handler(event,context):
     s3_csv_file = conf.s3_csv_file
 
     # Download s3 csv file to lambda tmp folder. If csv is not exist in s3, create a csv file in temp folder
-    response = s3.list_objects_v2(Bucket=my_s3_bucket, Prefix=s3_csv_file)
+    response = s3.list_objects_v2(Bucket=my_s3_bucket,Prefix=s3_csv_file)
     if response.get('Contents'):
-        s3.download_file(my_s3_bucket, s3_csv_file, lambda_temp_file)
+        s3.download_file(my_s3_bucket, s3_csv_file,lambda_temp_file)
     else:
-        csv.writer(open(lambda_temp_file, "w+"))
+        csv.writer(open(lambda_temp_file,"w+"))
 
-    # Assigning GitHub acount username and token
+    # Assigning GitHub acount token
     github_obj = Github(conf.token)
     repositories = conf.repositories
-
-    # Calling GitHub function to get the Dunzo Repository stats
-    store_github_stats(github_obj,repositories)
-
-    # upload the lambda temp file to s3 bucket
-    s3.upload_file(lambda_temp_file, my_s3_bucket, s3_csv_file)
-
-    return {
-        'message': 'success!!'
-    }
-
-def store_github_stats(github_obj,repositories):
 
     # Converting GMT date time to IST date time since the lambda function uses GMT time standard
     date = datetime.datetime.today()
@@ -48,47 +36,72 @@ def store_github_stats(github_obj,repositories):
     today_date = date_ist.strftime("%Y-%m-%d")
     date_time = date_ist.strftime("%Y-%m-%d %H:%M:%S")
 
-    #Opening csv file to write the collected Github Stats.
-    with open(lambda_temp_file, 'a+', newline='') as f:
+    #Opening csv file from lambda tmp folder.
+    with open(lambda_temp_file,'a+',newline='') as f:
         write = csv.writer(f,delimiter=',')
 
         #If header row is not available, write the header row.
         if f.tell() == 0:
-            write.writerow(['Repository','Date', 'Starts', 'Forks', 'Today\'s Clones', 'Today\'s Unique Clones', 'Today\'s Views', 'Today\'s Unique Visitors', 'Fortnight Clones', 'Fortnight Unique Clones', 'Fortnight Views', 'Fortnight unique Views'])
+            write.writerow(['Repository', 'Date', 'Starts', 'Forks', 'Today\'s Clones', 'Today\'s Unique Clones', 'Today\'s Views', 'Today\'s Unique Visitors', 'Fortnight Clones', 'Fortnight Unique Clones', 'Fortnight Views', 'Fortnight unique Views'])
 
-        #Loop for the each listed repositories
         for repository in repositories:
-            repo = github_obj.get_repo(repository)
+            stars = get_repo_stars(github_obj,repository)
+            forks = get_repo_forks(github_obj,repository)
+            clone_dict,clone_count,unique_clone_count = get_repo_clone(today_date,github_obj,repository)
+            visitors_dict,view_count,unique_visitors = get_repo_views(today_date,github_obj,repository)
 
-            #Collect the number of stars the repo contains
-            stars = repo.stargazers_count
-
-            #Collect the number of forks the repo contains
-            forks = repo.forks
-
-            #Collect the clone details and fetch today's clones by comparing with date.
-            clone_value = repo.get_clones_traffic()
-            clones = clone_value['clones']
-            for clone in clones:
-                if str(today_date) in str(clone.timestamp):
-                    clone_count = clone.count
-                    unique_clone_count = clone.uniques
-                    break
-                else: 
-                    clone_count = 0
-                    unique_clone_count = 0
-
-            #Collect the view details and fetch today's views by comparing with date.
-            visitors_value = repo.get_views_traffic()
-            views = visitors_value['views']
-            for view in views:
-                if str(today_date) in str(view.timestamp):
-                    view_count = view.count
-                    unique_visitors = view.uniques
-                    break
-                else: 
-                    view_count = 0
-                    unique_visitors = 0
-    
             #Writing the collected values to csv file
-            write.writerow([repository, date_time, stars, forks, clone_count, unique_clone_count, view_count, unique_visitors, clone_value['count'], clone_value['uniques'], visitors_value['count'], visitors_value['uniques']])
+            write.writerow([repository, date_time, stars, forks, clone_count, unique_clone_count, view_count, unique_visitors, clone_dict['count'], clone_dict['uniques'], visitors_dict['count'], visitors_dict['uniques']])
+
+    # upload the lambda temp file to s3 bucket
+    s3.upload_file(lambda_temp_file,my_s3_bucket,s3_csv_file)
+
+    return {
+        'message': 'success!!'
+    }
+
+def get_repo_stars(github_obj,repository):
+    "Return the number of stars the repo contains"
+    repo = github_obj.get_repo(repository)
+    stars = repo.stargazers_count
+
+    return stars
+
+def get_repo_forks(github_obj,repository):
+    "Return the number of forks the repo contains"
+    repo = github_obj.get_repo(repository)
+    forks = repo.forks_count
+
+    return forks
+
+def get_repo_clone(today_date,github_obj,repository):
+    "Return the number of clones and unique clones the repo contains"
+    repo = github_obj.get_repo(repository)
+    clone_dict = repo.get_clones_traffic()
+    clones = clone_dict['clones']
+    for clone in clones:
+        if str(today_date) in str(clone.timestamp):
+            clone_count = clone.count
+            unique_clone_count = clone.uniques
+            break
+        else: 
+            clone_count = 0
+            unique_clone_count = 0
+
+    return clone_dict,clone_count,unique_clone_count
+
+def get_repo_views(today_date,github_obj,repository):
+    "Return the number of visitors and unique views the repo contains"
+    repo = github_obj.get_repo(repository)
+    visitors_dict = repo.get_views_traffic()
+    views = visitors_dict['views']
+    for view in views:
+        if str(today_date) in str(view.timestamp):
+            view_count = view.count
+            unique_visitors = view.uniques
+            break
+        else: 
+            view_count = 0
+            unique_visitors = 0
+            
+    return visitors_dict,view_count, unique_visitors
